32b0eedc8ceee270f91a999cd2dbc698365ed534
Gustavo Nascimento <ngustavo80@hotmail.com>
2021-07-01 10:13:25 -0300

    Home page and button context crated

---
+++ src/pages/Home.tsx
@@ -0,0 +1,37 @@
+import { Button } from '../components/Button'
+
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import googleIcon from '../assets/images/google-icon.svg'
+
+import '../styles/auth.scss'
+
+export function Home() {
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Home page illustration" />
+                <strong>Crie salas de Q&amp;A ao vivo</strong>
+                <p>Tire as dúvidas da sua audiência em tempo real</p>
+            </aside>
+
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="App's logo letmeask" />
+                    <button className="create-room">
+                        <img src={googleIcon} alt="Google's logo" />
+                        Crie sua sala com Google
+                    </button>
+
+                    <div className="separator">ou entre em uma sala</div>
+                    <form>
+                        <input type="text" placeholder="Digite o código da sala"/>
+                        <Button type="submit">
+                            Entrar na sala
+                        </Button>
+                    </form>
+                </div>
+            </main>
+        </div>
+    )
+}
